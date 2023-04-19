c1e126883b6e50011b69c469b62fd5a90b3c0f33
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2023-04-20 00:21:03 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,16 +4,16 @@ Pascal triangle
 """
 
 def pascal_triangle(n):
+    triangle []
     if n <= 0:
         return []
 
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            elif i > 0 and j > 0:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         triangle.append(row)
-
     return triangle
